1f2dc6776e10b313d0631fbc896957065825762c
CoveredExpert <74656823+CoveredExpert@users.noreply.github.com>
2021-05-17 15:46:32 +0530

    Add files via upload

---
+++ PRO-C43-Template-main/js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("viridian")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("N U T O N I F C", 700,150)
+    fill("lightblue")
+    text("Hint: Performs a particular task!!", 700,170)
+    
+    fill("white")
+    textSize(15)
+    text("A B A A E S T D", 100,250)
+    fill("lightblue")
+    text("Hint:Stores all data", 100,270)
+}
